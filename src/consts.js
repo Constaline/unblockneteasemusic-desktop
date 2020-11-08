6c179ee777ba25f95d94c2a2a7f380a13545b55d
Constaline <constalline@gmail.com>
2020-11-08 16:38:28 +0800

    update tray menu

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,6 @@
 const {app} = require('electron')
 const path = require('path')
+const packageJSON = require('../package.json')
 
 global.ROOT_PATH = path.join(__dirname, '../')
 global.ICON_PATH = path.join(ROOT_PATH, './resources/logo.png')
@@ -8,7 +9,8 @@ global.appName = 'UnblockNeteaseMusic';
 global.appInfo = {
   name: 'UnblockNeteaseMusic',
   version: app.getVersion(),
-  platform: process.platform
+  platform: process.platform,
+  repository: packageJSON.repository
 };
 
 global.tray = null;
--- src/tray.js
+++ src/tray.js
@@ -1,36 +1,44 @@
-const {app, Menu, Tray} = require('electron')
+const {app, Menu, Tray, shell} = require('electron')
+
+const contextMenu = Menu.buildFromTemplate([
+  {
+    label: `${global.appInfo.name} v${appInfo.version}`,
+    click: () => {
+      shell.openExternal(global.appInfo.repository.url)
+    }
+  },
+  {
+    type: 'separator' 
+  },
+  {
+    type : 'checkbox',
+    label: 'OpenAtLogin',
+    checked: app.getLoginItemSettings().openAtLogin,
+    click: () => {
+      if(!app.isPackaged){ return; }
+      app.setLoginItemSettings({
+        openAtLogin: !app.getLoginItemSettings().openAtLogin,
+      })
+    }
+  },
+  { type: 'separator' },
+  { 
+    label: 'Relaunch', 
+    click: () => {
+      app.relaunch();
+      app.exit(0);
+    }
+  },
+  { 
+    label: 'Exit', 
+    click: () => {
+      app.quit()  
+    }
+  }
+]);
 
 const createTray = () => {
   tray = new Tray(ICON_PATH)
-  const contextMenu = Menu.buildFromTemplate([
-    { label: `${global.appInfo.name} v${appInfo.version}`},
-    { type: 'separator' },
-    {
-      type : 'checkbox',
-      label: 'OpenAtLogin',
-      checked: app.getLoginItemSettings().openAtLogin,
-      click: () => {
-        if(!app.isPackaged){ return; }
-        app.setLoginItemSettings({
-          openAtLogin: !app.getLoginItemSettings().openAtLogin,
-        })
-      }
-    },
-    { type: 'separator' },
-    { 
-      label: 'Relaunch', 
-      click: () => {
-        app.relaunch();
-        app.exit(0);
-      }
-    },
-    { 
-      label: 'Exit', 
-      click: () => {
-        app.quit()  
-      }
-    }
-  ])
   tray.setToolTip(global.appInfo.name)
   tray.setContextMenu(contextMenu)
 }
