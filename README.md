ef7f539c632bf3db9b38d213fc57f6e3e7d19da2
Constaline <constalline@gmail.com>
2020-11-08 15:39:57 +0800

    fix startUnblockServer

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ UnblockNeteaseMusic Windows Desktop version.
 ## Usage
 1. Run and it listens at port 16163 (default).
 2. Open NeteaseMusic client, set proxy to IP 127.0.0.1, port 16163.
-3. Save and restart the client. Enjoy!
+3. Save and restart the NeteaseMusic client. Enjoy!
 
 ## Thanks
 * [nondanee/UnblockNeteaseMusic](https://github.com/nondanee/UnblockNeteaseMusic)
--- src/server.js
+++ src/server.js
@@ -1,15 +1,21 @@
+const {app} = require('electron')
 const {notify} = require('./notifier')
 const {setTrayToolTip} = require('./tray')
-
-const showNotification = () => {
-  let url = `http://${global.address || '0.0.0.0'}:${global.port}`
-  notify({ message: `Server running @ ${url}` })
-  setTrayToolTip(`${global.appName} @ ${url}`)
-}
+const startUnblockServer = require('@nondanee/unblockneteasemusic/src/app');
 
 const startServer = () => {
-  require('@nondanee/unblockneteasemusic/app')
-  showNotification()
+  startUnblockServer()
+    .then(() => {
+      let url = `http://${global.address || '0.0.0.0'}:${global.port}`
+      notify({ message: `Server is running @ ${url}` })
+      setTrayToolTip(`${global.appName} @ ${url}`)
+    })
+    .catch(error => {
+      notify({ message: `Server error: ${error}`})
+        .then(() => {
+          app.quit()
+        })
+    })
 };
 
 module.exports = {
