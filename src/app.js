16a6b12538d1bb287c88012fea2dfdfbebb569bc
Constaline <constalline@gmail.com>
2020-11-08 14:53:30 +0800

    second instance notify

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const {app} = require('electron')
 const {createTray} = require('./tray')
 const {startServer} = require('./server')
+const {notify} = require('./notifier')
 
 const instanceLock = app.requestSingleInstanceLock()
 
 if (!instanceLock) {
   app.quit()
 } else {
+  app.on('second-instance', () => {
+    notify({message: 'An instance is already running'})
+  })
+
   app.whenReady().then(() => {
     createTray()
     startServer()
