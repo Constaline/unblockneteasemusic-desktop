5656be8ff3ca1cb21faa63d62568405c9514fdf4
Constaline <constalline@gmail.com>
2021-05-02 08:09:24 +0800

    provider/bilibili: add(nondanee/UnblockNeteaseMusic#711)

--- src/consts.js
+++ src/consts.js
@@ -24,7 +24,7 @@ global.userConfig = {}
 let configStoreOptions = {
   defaults: {
     '__instruction__': 'After configuaration, please relaunch the app.',
-    '__source_list__': `['qq', 'kuwo', 'migu', 'xiami', 'baidu', 'kugou', 'joox', 'youtube']`,
+    '__source_list__': `['qq', 'kuwo', 'migu', 'xiami', 'baidu', 'kugou', 'joox', 'youtube', 'bilibili']`,
     source: ['qq', 'kuwo', 'migu'],
     port: 16163
   },
